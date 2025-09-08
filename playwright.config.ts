005191c8325662ae7009f3966e5264d3637f4e96
purwarshubham <purwarshubham@gmail.com>
2025-09-09 00:58:01 +0530

    allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -23,9 +24,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'], // pretty console output
-    ['json', { outputFile: 'results.json' }], // JSON for parsing in GitHub Actions
-    ['html', { outputFolder: 'playwright-report', open: 'never' }], // HTML report
+    ['list'],
+    ['json', { outputFile: path.join(__dirname, 'results.json') }], // absolute path
+    ['html', { outputFolder: path.join(__dirname, 'playwright-report'), open: 'never' }],
     ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
