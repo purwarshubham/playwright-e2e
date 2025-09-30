86a6d8764fff6a12fa173211dbd026fa3296137f
purwarshubham <purwarshubham@gmail.com>
2025-09-30 13:27:11 +0530

    allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     ['list'], // pretty console output
     ['json', { outputFile: 'results.json' }], // JSON for parsing in GitHub Actions
     ['html', { outputFolder: 'playwright-report', open: 'never' }], // HTML report
-    ['allure-playwright', { outputFolder: 'allure-results' }]
+    ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
